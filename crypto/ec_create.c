7f022ecf5744554a24d9a71079684530cb3c6c89
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-03-26 00:14:40 +0000

    Second Task EC_KEY creation

---
+++ crypto/ec_create.c
@@ -0,0 +1,13 @@
+#include "hblk_crypto.h"
+
+EC_KEY *ec_create(void){
+	EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
+	
+	if(!key)
+		return NULL;
+
+	if(!EC_KEY_generate_key(key))
+		return NULL;
+
+	return key;
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,15 +1,17 @@
 #include <openssl/sha.h>
 #include <stdint.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
 
-#define EC_CURVE   NID_secp256k1
+#define EC_CURVE NID_secp256k1
 
 /* EC_KEY public key octet string length (using 256-bit curve) */
 #define EC_PUB_LEN 65
 /* Maximum signature octet string length (using 256-bit curve) */
-#define SIG_MAX_LEN    72
+#define SIG_MAX_LEN 72
 
-#define PRI_FILENAME   "key.pem"
-#define PUB_FILENAME   "key_pub.pem"
+#define PRI_FILENAME "key.pem"
+#define PUB_FILENAME "key_pub.pem"
 
 /**
  * struct sig_s - EC Signature structure
@@ -23,13 +25,11 @@ typedef struct sig_s
      * @sig must stay first, so we can directly use the structure as
      * an array of char
      */
-    uint8_t     sig[SIG_MAX_LEN];
-    uint8_t     len;
+    uint8_t sig[SIG_MAX_LEN];
+    uint8_t len;
 } sig_t;
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
 
-
-
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
 
