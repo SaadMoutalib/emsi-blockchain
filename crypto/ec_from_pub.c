4c76a51031ded416dfdd82d6366106c9883de9d9
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-03-28 16:48:16 +0000

    Fourth Task + Third Task fix

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,9 +1,31 @@
 #include "hblk_crypto.h"
 
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]){
-	EC_KEY *key = EC_KEY_new_by_curve_name(NID_secp256k1);
-	BIGNUM *x = BN_bin2bn(pub);
-	EC_KEY_set_public_key_affine_coordinates(key, x, NULL);
-	BN_free(x);
+	EC_KEY *key;
+	const EC_GROUP *group;
+	EC_POINT *pub_key;
+
+	if(!pub)
+		return NULL;
+
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+  	group = EC_KEY_get0_group(key);
+  	pub_key = EC_POINT_new(group);
+	
+	if(!EC_POINT_oct2point(group, pub_key, pub, EC_PUB_LEN, NULL)){
+		EC_KEY_free(key);
+		EC_POINT_free(pub_key);
+		return NULL;
+	}
+
+	if(!EC_KEY_set_public_key(key, pub_key))
+	{	
+		EC_KEY_free(key);
+		EC_POINT_free(pub_key);
+		return NULL;
+	}
+
+	EC_POINT_free(pub_key);
+
 	return key;
 }
--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -3,21 +3,11 @@
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]){
 	const EC_POINT* point = EC_KEY_get0_public_key(key);
 	const EC_GROUP* group = EC_KEY_get0_group(key);
-		
-	BIGNUM *x = BN_new();	
-	BIGNUM *y = BN_new();
-
 	if(!group || !point)
 		return NULL;	
 	
-	if (EC_POINT_get_affine_coordinates_GFp(group, point, x, y, NULL)) {
-		BN_bn2bin(x, pub);
-	}else{
+	if (!EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))	
 		return NULL;
-	}
-	
-	BN_free(x);
-	BN_free(y);
 	
 	return pub;
 }
