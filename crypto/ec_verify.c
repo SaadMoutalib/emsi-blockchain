1b2aebe7728144deec0202bd353c61c7f95b62e6
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-03-31 02:36:38 +0000

    Eight Tast : Signature verification

---
+++ crypto/ec_verify.c
@@ -0,0 +1,17 @@
+#include "hblk_crypto.h"
+
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig){
+
+	EC_KEY *ckey = EC_KEY_new();
+
+	if(!key || !msg || !sig)
+		return 0;
+
+	if(!EC_KEY_copy(ckey, key))
+		return 0;
+
+	if(ECDSA_verify(0, msg, msglen, sig->sig, sig->len, ckey)==0)
+		return 0;
+	
+	return 1;
+}
