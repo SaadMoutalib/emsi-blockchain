84e18332d503d666d9236c97127c6578ee39a604
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-17 15:03:39 +0000

    Fixed style

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,31 +1,31 @@
 #include "hblk_crypto.h"
 
-EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]){
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
 	EC_KEY *key;
 	const EC_GROUP *group;
 	EC_POINT *pub_key;
 
-	if(!pub)
-		return NULL;
-
+	if (!pub)
+		return (NULL);
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
-  	group = EC_KEY_get0_group(key);
-  	pub_key = EC_POINT_new(group);
-	
-	if(!EC_POINT_oct2point(group, pub_key, pub, EC_PUB_LEN, NULL)){
+	group = EC_KEY_get0_group(key);
+	pub_key = EC_POINT_new(group);
+	if (!EC_POINT_oct2point(group, pub_key, pub, EC_PUB_LEN, NULL))
+	{
 		EC_KEY_free(key);
 		EC_POINT_free(pub_key);
-		return NULL;
+		return (NULL);
 	}
 
-	if(!EC_KEY_set_public_key(key, pub_key))
-	{	
+	if (!EC_KEY_set_public_key(key, pub_key))
+	{
 		EC_KEY_free(key);
 		EC_POINT_free(pub_key);
-		return NULL;
+		return (NULL);
 	}
 
 	EC_POINT_free(pub_key);
 
-	return key;
+	return (key);
 }
