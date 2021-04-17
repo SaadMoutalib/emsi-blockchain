e18a15d04104fece95cc34fed92f4dff174de166
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-17 13:42:47 +0000

    Fixed Style

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -9,16 +9,16 @@
  * Return: pointer to digest, NULL on error
  */
 uint8_t *sha256(int8_t const *s, size_t len,
-		uint8_t digest[SHA256_DIGEST_LENGTH])
+	uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	SHA256_CTX context;
-	
-	if(!s || !digest)
-		return NULL;
 
+	if (!s || !digest)
+	{
+		return (NULL);
+	}
 	SHA256_Init(&context);
 	SHA256_Update(&context, s, len);
-	SHA256_Final(digest ,&context);
-	
-	return digest;
+	SHA256_Final(digest, &context);
+	return (digest);
 }
