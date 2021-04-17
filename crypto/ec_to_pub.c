436dd535a5fb7778abee126179e7f232192b0545
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-17 14:37:23 +0000

    Fixed NULL case

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -9,13 +9,15 @@
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-	const EC_POINT *point = EC_KEY_get0_public_key(key);
-	const EC_GROUP *group = EC_KEY_get0_group(key);
+	const EC_POINT *point;
+	const EC_GROUP *group;
 
 	if (!key || !pub)
 	{
 		return (NULL);
 	}
+	point = EC_KEY_get0_public_key(key);
+	group = EC_KEY_get0_group(key);
 	if (!EC_POINT_point2oct(group, point, POINT_CONVERSION_UNCOMPRESSED,
 				pub, EC_PUB_LEN, NULL))
 	{
