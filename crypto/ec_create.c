d5b40d97885f1bcef182d30a528ce7b460ff2886
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-17 14:13:17 +0000

    Fixed style

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,12 +1,16 @@
 #include "hblk_crypto.h"
 
-EC_KEY *ec_create(void){
-	EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
-	
-	if(!key)
-		return NULL;
+EC_KEY *ec_create(void)
+{
+	C_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
 
-	if(!EC_KEY_generate_key(key))
-		return NULL;
-	return key;
+	if (!key)
+	{
+		return (NULL);
+	}
+	if (!EC_KEY_generate_key(key))
+	{
+		return (NULL);
+	}
+	return (key);
 }
