991b951542c1789b97ad795911e95dbda4e7079b
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-17 14:31:03 +0000

    Fixed betty doc

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,11 +1,18 @@
 #include "hblk_crypto.h"
 
+/**
+ * ec_to_pub - extract public key from EC_KEY struct
+ * @key: pointer to EC_KEY struct to extract from
+ * @pub: address to store extracted public key
+ *
+ * Return: pointer to pub, NULL on error
+ */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
 	const EC_POINT *point = EC_KEY_get0_public_key(key);
 	const EC_GROUP *group = EC_KEY_get0_group(key);
 
-	if (!group || !point)
+	if (!key || !pub)
 	{
 		return (NULL);
 	}
