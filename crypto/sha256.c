479205c859e92fc988d82dee00036c95ca38c029
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-04-17 12:11:25 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -5,5 +5,5 @@ uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH
 	if(!digest)
 		return NULL;
 	
-	return SHA256(s, len, digest);
+	return (SHA256((const unsigned char *)s, len, digest));
 }
