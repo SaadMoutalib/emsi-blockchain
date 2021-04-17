3d3033deb31c78855ef49473472e1900bb361bfc
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-17 15:30:03 +0000

    Fixed style

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,31 +1,31 @@
 #include "hblk_crypto.h"
 
-EC_KEY *ec_load(char const *folder){
+EC_KEY *ec_load(char const *folder)
+{
 	char path[BUFSIZ];
 	EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
-	
 	FILE *f;
-	
-	if(!key || !folder)
-		return NULL;
-	
+
+	if (!key || !folder)
+		return (NULL);
+
 	sprintf(path, "%s/%s", folder, PUB_FILENAME);
 	f = fopen(path, "r");
 
-	if(!f)
-		return NULL;
+	if (!f)
+		return (NULL);
 
-	if(!PEM_read_EC_PUBKEY(f, &key, NULL, NULL))
-		return NULL;
+	if (!PEM_read_EC_PUBKEY(f, &key, NULL, NULL))
+		return (NULL);
 
 	sprintf(path, "%s/%s", folder, PRI_FILENAME);
 	f = fopen(path, "r");
 
-	if(!f)
-		return NULL;
+	if (!f)
+		return (NULL);
 
-	if(!PEM_read_ECPrivateKey(f, &key, NULL, NULL))
-		return NULL;
-	
-	return key;
+	if (!PEM_read_ECPrivateKey(f, &key, NULL, NULL))
+		return (NULL);
+
+	return (key);
 }
