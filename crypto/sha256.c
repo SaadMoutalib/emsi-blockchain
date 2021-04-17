c79bc1be64204662bd15dd65f67bb2ee3c9412f9
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-04-17 12:07:43 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -3,13 +3,13 @@
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]){
 	SHA256_CTX context;
-
-	SHA256_Init(&context);
-	SHA256_Update(&context, s, len);
-	SHA256_Final(digest ,&context);
 	
 	if(!digest)
 		return NULL;
+	
+	SHA256_Init(&context);
+	SHA256_Update(&context, s, len);
+	SHA256_Final(digest ,&context);
 	
 	return digest;
 }
