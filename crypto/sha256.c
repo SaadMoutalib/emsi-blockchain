0e8f45ea5f9c618e1690fb28b660ab78ee399fec
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-04-17 12:10:50 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,14 +2,8 @@
 #include <openssl/sha.h>
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]){
-	SHA256_CTX context;
-	
 	if(!digest)
 		return NULL;
 	
-	SHA256_Init(&context);
-	SHA256_Update(&context, s, len);
-	SHA256_Final(digest ,&context);
-	
-	return digest;
+	return SHA256(s, len, digest);
 }
