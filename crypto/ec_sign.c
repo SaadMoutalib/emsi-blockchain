7e1097148336ba035189a386fd9be526eab897eb
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-21 15:50:37 +0000

    Fix

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -12,9 +12,12 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,	size_t msglen,
 		sig_t *sig)
 {
+	unsigned char hash[SHA256_DIGEST_LENGTH];	
 	if (!key || !msg || !sig)
 		return (NULL);
-	if (!ECDSA_sign(0, msg, msglen, sig->sig,
+	if (!SHA256(msg, msglen, hash))
+		return (NULL);
+	if (!ECDSA_sign(0, hash, SHA256_DIGEST_LENGTH, sig->sig,
 				(unsigned int *)&(sig->len), (EC_KEY *)key))
 		return (NULL);
 	return (sig->sig);
