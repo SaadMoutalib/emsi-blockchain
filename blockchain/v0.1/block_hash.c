2b6df5e6b4bb31b262f8326fb243533c91bf6c2d
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-22 14:34:51 +0000

    Task 4: Hash Block

---
+++ blockchain/v0.1/block_hash.c
@@ -0,0 +1,12 @@
+#include "blockchain.h"
+
+/**
+ * block_hash - computes the hash of a Block
+ * @block:points to the Block to be hashed
+ *
+ * Return: pointer to hash_buf
+ */
+uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	return (SHA256((const unsigned char *)block, sizeof(block), hash_buf));
+}
