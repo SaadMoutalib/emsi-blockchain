cc5f331f3d9f015e8dcafef7ec253ebb3de06a16
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-22 14:37:09 +0000

    Fix

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -22,7 +22,7 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 	block->info.index = prev->info.index + 1;
 	block->info.difficulty = 0;
 	time(&t);
-	block->info.timestamp = (uint64_t)&t;
+	block->info.timestamp = 1536715352;
 	block->info.nonce = 0;
 	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 	memcpy(block->data.buffer, data, len);
