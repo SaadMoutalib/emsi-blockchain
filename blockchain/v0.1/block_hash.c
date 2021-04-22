f6c615a79a005baf27cb107da71cb5c2be107619
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-22 15:04:11 +0000

    Fix

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -2,11 +2,13 @@
 
 /**
  * block_hash - computes the hash of a Block
- * @block:points to the Block to be hashed
- *
+ * @block: points to the Block to be hashed
+ * @hash_buf: stores the resulting hash
  * Return: pointer to hash_buf
  */
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(block_t const *block,
+			uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	return (SHA256((const unsigned char *)block, sizeof(block), hash_buf));
+	return (SHA256((const unsigned char *)block,
+		sizeof(block), hash_buf));
 }
