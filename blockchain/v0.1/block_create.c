c0d0838355555b93721fc80212a12bb57a4d2272
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-29 13:52:06 +0000

    Fix

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -13,15 +13,13 @@ block_t *block_create(block_t const *prev, int8_t const *data,
 {
 	block_t *block;
 	uint32_t len;
-	time_t t;
 
 	len = data_len > BLOCKCHAIN_DATA_MAX ?
 		BLOCKCHAIN_DATA_MAX : data_len;
 
-	block  = (block_t *)malloc(sizeof(block_t));
+	block  = (block_t *)calloc(1, sizeof(block_t));
 	block->info.index = prev->info.index + 1;
 	block->info.difficulty = 0;
-	time(&t);
 	block->info.timestamp = time(NULL);
 	block->info.nonce = 0;
 	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
