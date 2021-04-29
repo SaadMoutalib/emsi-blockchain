0277efc7d121e11906600ba15340a595dc35dc11
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-29 13:10:11 +0000

    Fix

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -13,14 +13,20 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,	size_t msglen,
 		sig_t *sig)
 {
 	EC_KEY *ckey = EC_KEY_new();
+	unsigned char hash[SHA256_DIGEST_LENGTH];
 
 	if (!key || !msg || !sig)
 		return (NULL);
 	if (!EC_KEY_check_key(key))
 		return (NULL);
+	if (!SHA256(msg, msglen, md))
+		return (NULL);
+	sig->len = ECDSA_size(key);
+	if (!sig->len)
+		return (NULL);
 	if (!EC_KEY_copy(ckey, key))
 		return (NULL);
-	if (!ECDSA_sign(0, msg, msglen, sig->sig,
+	if (!ECDSA_sign(0, hash, SHA256_DIGEST_LENGTH, sig->sig,
 				(unsigned int *)&(sig->len), ckey))
 		return (NULL);
 	EC_KEY_free(ckey);
