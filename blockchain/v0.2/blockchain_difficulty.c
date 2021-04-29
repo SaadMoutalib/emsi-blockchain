fa4aedb90054af1befe9f02f930bb927266bda33
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-29 17:56:42 +0000

    Fix: conversion problem

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -1,5 +1,4 @@
 #include "blockchain.h"
-#include <inttypes.h>
 
 /**
  * blockchain_difficulty - computes the difficulty to assign to the next Block
@@ -9,25 +8,31 @@
  */
 uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 {
-	block_t *last_block, *adjusted_block;
-	uint64_t expected_time, actual_time;
+	block_t *last_block, *last_adjusted_block;
+	uint64_t exp_elapsed_time, act_elapsed_time;
 
 	if (!blockchain)
 		return (0);
+
 	last_block = llist_get_tail(blockchain->chain);
+
 	if (!last_block)
 		return (0);
-	if (last_block->info.index == 0 ||
-		last_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL)
-		return (last_block->info.difficulty);
-	adjusted_block = llist_get_node_at(blockchain->chain,
-		last_block->info.index + 1 - DIFFICULTY_ADJUSTMENT_INTERVAL);
-	expected_time = DIFFICULTY_ADJUSTMENT_INTERVAL * BLOCK_GENERATION_INTERVAL;
-	actual_time = last_block->info.timestamp - adjusted_block->info.timestamp;
-	if (actual_time * 2 < expected_time)
-		return (last_block->info.difficulty + 1);
-	else if (actual_time > 2 * expected_time)
-		return (last_block->info.difficulty > 0 ?
-			last_block->info.difficulty - 1 : 0);
+
+	if (last_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL == 0 &&
+		last_block->info.index != 0)
+	{
+		last_adjusted_block = llist_get_node_at(blockchain->chain,
+			llist_size(blockchain->chain) - DIFFICULTY_ADJUSTMENT_INTERVAL);
+		exp_elapsed_time = BLOCK_GENERATION_INTERVAL *
+					DIFFICULTY_ADJUSTMENT_INTERVAL;
+		act_elapsed_time = last_block->info.timestamp -
+					last_adjusted_block->info.timestamp;
+		if (act_elapsed_time * 2 < exp_elapsed_time)
+			return (last_block->info.difficulty + 1);
+		else if (act_elapsed_time > exp_elapsed_time * 2)
+			return (last_block->info.difficulty > 0 ?
+				last_block->info.difficulty - 1 : 0);
+	}
 	return (last_block->info.difficulty);
 }
