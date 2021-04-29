811a4e0d177cfe24d508ff3df74dac4453248637
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-29 15:30:58 +0000

    Fix

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -34,11 +34,25 @@
 } \
 }
 
+/**
+ * struct blockchain_s - Blockchain structure
+ *
+ * @chain: Linked list of pointers to block_t
+ */
 typedef struct blockchain_s
 {
 llist_t     *chain;
 } blockchain_t;
 
+/**
+ * struct block_info_s - Block info structure
+ *
+ * @index:	  Index of the Block in the Blockchain
+ * @difficulty: Difficulty of proof of work (hash leading zero bits)
+ * @timestamp:  Time the Block was created at (UNIX timestamp)
+ * @nonce:	  Salt value used to alter the Block hash
+ * @prev_hash:  Hash of the previous Block in the Blockchain
+ */
 typedef struct block_info_s
 {
 uint32_t    index;
@@ -49,6 +63,12 @@ uint8_t     prev_hash[SHA256_DIGEST_LENGTH];
 
 } block_info_t;
 
+/**
+ * struct block_data_s - Block data
+ *
+ * @buffer: Data buffer
+ * @len:	Data size (in bytes)
+ */
 typedef struct block_data_s
 {
 int8_t      buffer[BLOCKCHAIN_DATA_MAX];
@@ -56,6 +76,13 @@ uint32_t    len;
 
 } block_data_t;
 
+/**
+ * struct block_s - Block structure
+ *
+ * @info: Block info
+ * @data: Block data
+ * @hash: 256-bit digest of the Block, to ensure authenticity
+ */
 typedef struct block_s
 {
 block_info_t    info;
