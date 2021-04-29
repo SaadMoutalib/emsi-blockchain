d8dd7be420cabb109239d67d71b1fb6672102b4d
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-04-29 13:19:18 +0000

    Fix

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -16,7 +16,7 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,	size_t msglen,
 		return (NULL);
 	if (!EC_KEY_check_key(key))
 		return (NULL);
-	if (!ECDSA_sign(0, msg, msglen, sig->sig,
+	if (!ECDSA_sign(EC_CURVE, msg, msglen, sig->sig,
 				(unsigned int *)&(sig->len), (EC_KEY *)key))
 		return (NULL);
 	return (sig->sig);
