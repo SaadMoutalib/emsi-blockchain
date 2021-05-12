3d060166c72362f3af0500fcbca1a957dba87a92
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-05-12 23:24:57 +0000

    Task 13. Update: Block validation

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -221,7 +221,8 @@ void _swap_endian(void *p, size_t size);
 
 blockchain_t *blockchain_deserialize(char const *path);
 
-int block_is_valid(block_t const *block, block_t const *prev_block);
+int block_is_valid(block_t const *block, block_t const *prev_block,
+					llist_t *all_unspent);
 
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 	uint32_t difficulty);
