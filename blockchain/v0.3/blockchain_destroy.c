850a836c1d55a8377059abdc4b1b35b79f084a29
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-05-12 22:54:29 +0000

    Task 10. Update Blockchain and Block creation/deletion

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -9,7 +9,8 @@ void blockchain_destroy(blockchain_t *blockchain)
 {
 	if (!blockchain)
 		return;
-	llist_destroy(blockchain->unspent, 1, free);
+	if (blockchain->unspent)
+		list_destroy(blockchain->unspent, 1, free);
 	llist_destroy(blockchain->chain, 1,
 		(void (*)(llist_node_t))block_destroy);
 	free(blockchain);
