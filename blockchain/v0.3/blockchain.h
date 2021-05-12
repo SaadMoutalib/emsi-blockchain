4f3f283a8eaaf94ef54d1ac356ba4f95da25eb7c
SaadMoutalib <saad-moutalib@hotmail.fr>
2021-05-12 17:06:30 +0000

    Added structs

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -164,6 +164,40 @@ typedef struct unspent_tx_out_s
     tx_out_t    out;
 } unspent_tx_out_t;
 
+/**
+ * struct Visitor - visitor struct for collect sender's unspent
+ * @sender_unspent: list to collect sender's unspent tx
+ * @sender_pub: sender's public key
+ * @total_amount: of unspent tx
+ * @amount: amount to send
+ */
+typedef struct Visitor
+{
+	llist_t *sender_unspent;
+	uint8_t *sender_pub;
+	uint64_t total_amount;
+	uint64_t amount;
+} visitor_t;
+
+/**
+ * struct Validation_Visitor - visitor struct for tx validation
+ * @in_amount: total txi amount
+ * @out_amount: total txo amount
+ * @valid: 1 if tx valid else 0
+ * @all_unspent: all unspent txs
+ * @tx: the tx to validate
+ * @block_index: the block index cointaining tx
+ */
+typedef struct Validation_Visitor
+{
+	long in_amount;
+	long out_amount;
+	int valid;
+	llist_t *all_unspent;
+	transaction_t const *tx;
+	uint32_t block_index;
+} validation_vistor_t;
+
 extern block_t const _genesis;
 
 blockchain_t *blockchain_create(void);
